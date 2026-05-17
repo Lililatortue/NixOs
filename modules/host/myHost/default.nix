a2ec299da4b3193d994b61511a049d845f858cc7
System administrator <will.descoteaux@gmail.com>
2026-05-16 22:55:15 -0400

    allow unfree in configuration

--- modules/host/myHost/default.nix
+++ modules/host/myHost/default.nix
@@ -1,16 +1,19 @@
 { self, inputs, ... }: 
 {
+      perSystem = { system, ... }: {
+            _module.args.pkgs = import inputs.nixpkgs {
+                  inherit system;
+                  config.allowUnfree = true; # <-- Unlocks unfree for flake-parts modules!
+            };
+      };
   flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem
   {
       specialArgs = { inherit self inputs; };
       modules = [
-      {
-        nixpkgs.pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-      }
-            self.nixosModules.myMachineConfiguration
-    ];
+           self.nixosModules.myMachineConfiguration
+           {
+            nixpkgs.config.allowUnfree = true;
+           }
+      ];
   };
 }
